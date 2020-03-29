f329875e55b3420fe7bfb961ed998ebc98fcb65e
azu <azuciao@gmail.com>
2020-03-29 10:12:50 +0900

    chore(deps): update mocha

--- test/assert-json-equal.test.ts
+++ test/assert-json-equal.test.ts
@@ -1,9 +1,9 @@
 import { assertJsonEqual } from "../src/assert-json-equal";
 import * as assert from "assert";
 
-describe('assertJsonEqual', function () {
+describe("assertJsonEqual", function() {
     it("should compare objects with deepStrictEqual", () => {
-        assertJsonEqual({a: {b: {c: 1}}}, {a: {b: {c: 1}}});
+        assertJsonEqual({ a: { b: { c: 1 } } }, { a: { b: { c: 1 } } });
     });
     it("should ignore difference between undefined value and missing property", () => {
         const a = {};
